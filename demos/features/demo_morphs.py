2eca40ff6b6c242f59dd9e1c599f3c52e068ee0a
SourCherries <carl.michael.gaspar@icloud.com>
2024-07-09 15:46:35 +0400

    demos/features/demo_morphs.py also writes figure of novel combo demo -- WRFF on morph sequence

--- demos/features/demo_morphs.py
+++ demos/features/demo_morphs.py
@@ -1,31 +1,11 @@
 from pathlib import Path
-import copy
-import os
 
 import matplotlib.pyplot as plt
 import numpy as np
-from skimage.io import imsave
+from skimage.util import montage
 
 import fad as fd
 
-
-# -----------------------------------------------
-# from .align.make_aligned_faces import get_source_files, get_landmarks, align_procrustes, place_aperture
-# from .align.make_files import clone_directory_tree
-
-# from .features.filters import face_filters, get_eo, eo_montage
-# from .features.roi import get_feature_roi
-# from .features.coefficients import select_coefficients_by_feature
-# from .features.reconstruct import wrff_progress as reconstruct
-# from .features.reconstruct import combine_features
-# from .features.shift_features import thatcher_face, space_out_features, space_out_ipd, double_face_illusion
-
-# -----------------------------------------------
-# from fad import align as af
-# af.make_aligned_faces.morph_between_two_faces
-
-# from fad.align.make_aligned_faces import morph_between_two_faces as morph
-# -----------------------------------------------
 
 # HERE = Path(__file__).resolve().parent
 HERE = Path("/Users/z10722/Tools/fad/demos/features")
@@ -34,32 +14,51 @@ stim_folder = HERE / "stimuli"
 fig_folder = HERE/ "figs"
 ID = ["122_03", "014_03"]  # 124_03
 
-# Basic usage
+# Perform morph on 2 faces from FRL
+num_morphs = 5
 bookends = ("","jpg")
 FRL = fd.Ensemble(dir_source = FRL_folder, file_bookends = bookends, INTER_PUPILLARY_DISTANCE = 64)
 FRL.list_faces()
 FRL.add_to_roster(ID[0])
 FRL.add_to_roster(ID[1])
-
 results = FRL.roster_morph_between(num_morphs=5)
 morph_path = results["morph_path"]
-# face_array, p, morph_path = morph(str(FRL.dir_aligned) + os.sep,
-#                                   do_these=[0, 1],
-#                                   num_morphs=num_morphs,
-#                                   file_prefix=FRL.file_bookends[0],
-#                                   file_postfix=FRL.file_bookends[1])
 
-
+# Wavelet features of morphs
 FRL = fd.Ensemble(dir_source = Path(morph_path), file_bookends = ("N","png"), make_windowed_faces=True, INTER_PUPILLARY_DISTANCE = 64)
-# ID = ["N"+str(i) for i in range(num_morphs)]
-# for i in range(num_morphs):
-#     FRL.add_to_roster(ID[i])
-FRL.add_all_to_roster()
+for i in range(num_morphs):
+    FRL.add_to_roster("N" + str(i))
+# FRL.add_all_to_roster()
+
 FRL.combine_roster_features()
 FRL.clip_roster_margins(margins=(1/6, 1/4))
 FRL.display_roster()
 
-# FRL = fd.Ensemble(dir_source = Path(morph_path), file_bookends = ("N","png"))
-# FRL.add_all_to_roster()
-# FRL.display_roster()
-# FRL.com
+# Make figure of morphs (WRFF)
+num_morphs = len(FRL.Roster)
+all_images = np.zeros((num_morphs*2, FRL.SHAPE[0], FRL.SHAPE[1], 4))
+for i in range(num_morphs):
+    source = FRL.Roster[i].img    
+    all_images[i,:,:,:] = fd.align.make_four_channel_image(source, FRL.aperture)
+for i in range(num_morphs):
+    wrff = FRL.Roster[i].F
+    all_images[i + num_morphs,:,:,:] = fd.align.make_four_channel_image(wrff, FRL.aperture)
+im_montage = montage(all_images, grid_shape=(2, num_morphs), channel_axis=3).astype(np.uint8)
+
+
+fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
+fig.set_size_inches(w=2*num_morphs, h=2)
+ax.imshow(im_montage, interpolation='nearest')
+
+ax.set_axis_off()
+plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0,
+            hspace = 0, wspace = 0)
+plt.margins(0,0)
+ax.xaxis.set_major_locator(plt.NullLocator())
+ax.yaxis.set_major_locator(plt.NullLocator())
+plt.savefig("figure-demos-features-morph.png", bbox_inches = 'tight',
+            pad_inches = 0)
+plt.show()
+
+# End
+# -------------------------------------------------------------------
